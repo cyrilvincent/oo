fc939e917796c0ceb05a59972bd978a9fbd077a7
Cyril Vincent <contact@cyrilvincent.com>
2023-06-05 11:25:58 +0200

    ST

--- reference.py
+++ reference.py
@@ -7,8 +7,14 @@ x = [1, 2]
 y = x
 x.append(3)
 print(x, y)
+print(x == y, x is y)
 
 n = [1, 2]
 m = list(n)
 n.append(3)
 print(n, m)
+print(n == m, n is m)
+
+g = [1, 2]
+h = [1, 2]
+print(g == h, g is h)
