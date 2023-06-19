7a1b30de223dc36194e46172812a6520e70198e3
Cyril Vincent <contact@cyrilvincent.com>
2023-06-19 11:14:58 +0200

    ST

---
+++ tp1.py
@@ -0,0 +1,12 @@
+from typing import List
+
+l1 = range(100)
+
+def filter_even(l: List[int]) -> List[int]:
+    pass
+
+x = filter_even(l1)
+# => 0,2,4,6,8...
+# Modulo operator = %
+print(4 % 2) # => 0
+print(5 % 2) # => 1
