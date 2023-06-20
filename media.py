0d6eed9e1a7852a3c5cdc9e0af08cb928fc0912d
Cyril Vincent <contact@cyrilvincent.com>
2023-06-20 10:45:33 +0200

    ST

--- media.py
+++ media.py
@@ -15,7 +15,7 @@ class Publisher:
     id: str
     name: str
 
-class Book:
+class Media:
 
 # Créer le constructeur, les attributs
 # net_price = price * 1.055
@@ -25,34 +25,64 @@ class Book:
 # Créer la dataclass Author
 # Créer la dataclass Publisher
 # Tester
-    nb_book = 0
+    nb_media = 0
 
     def __init__(self, id: str,
                  title: str,
                  price: float,
                  date: datetime.datetime = datetime.datetime.now(),
-                 nb_page: int = 0,
                  publisher: Publisher = Publisher("", ""),
                  authors: List[Author] = []):
         self.id = id
         self.title = title
         self.price = price
         self.date = date
-        self.nb_page = nb_page
         self.publisher = publisher
         self.authors = authors
-        Book.nb_book += 1
+        Media.nb_media += 1
 
     @property
     def net_price(self):
-        return self.price * 1.055
+        return self.price * 1.2
 
     def __del__(self):
-        Book.nb_book -= 1
+        Media.nb_media -= 1
+
+    def __repr__(self):
+        return f"Media: {self.id} {self.title} {self.price}"
+
+class Book(Media):
+
+    def __init__(self, id: str,
+                 title: str,
+                 price: float,
+                 date: datetime.datetime = datetime.datetime.now(),
+                 publisher: Publisher = Publisher("", ""),
+                 authors: List[Author] = [],
+                 nb_page = 0
+                 ):
+        super().__init__(id, title,price,date,publisher,authors)
+        self.nb_page = nb_page
+
+    def net_price(self):
+        return self.price * 1.055
+
+class Cd(Media):
+
+    def __init__(self, id: str,
+                 title: str,
+                 price: float,
+                 date: datetime.datetime = datetime.datetime.now(),
+                 publisher: Publisher = Publisher("", ""),
+                 authors: List[Author] = [],
+                 nb_track = 0
+                 ):
+        super().__init__(id, title,price,date,publisher,authors)
+        self.nb_track = nb_track
 
 b1 = Book("001", "Python", 10)
 print(b1)
-print(f"Book price: {b1.net_price:.2f}")
+print(f"Book price: {b1.net_price}")
 b2 = Book("002", "Numpy", 20, publisher=Publisher("123", "ENI"))
 b2.price+=1
 print(b2.price)
@@ -68,9 +98,12 @@ print(a1)
 b3 = Book("1234", "Les misérables", 5, authors=[])
 b3.authors.append(Author("toto", "titi"))
 
-print(f"Nb book: {Book.nb_book}")
+print(f"Nb media: {Media.nb_media}")
 del b3
-print(f"Nb book: {Book.nb_book}")
+print(f"Nb media: {Media.nb_media}")
+
+cd1 = Cd("0014", "Johnny", 10, nb_track=9)
+print(cd1)
 
 
 
